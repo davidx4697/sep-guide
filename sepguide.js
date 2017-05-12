096b7017a8fc334c5b22fea13e269be1edc060f9
davidx4697 <davidx4697@hstat.org>
2017-05-12 03:41:15 +0000

    fixed position for 9th

--- sepguide.js
+++ sepguide.js
@@ -1,25 +1,48 @@
-// jQuery to collapse the navbar on scroll
-function collapseNavbar() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
+jQuery(document).ready(function($) {
+    // jQuery to collapse the navbar on scroll
+    function collapseNavbar() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        }
+        else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
     }
-    else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-}
 
-$(window).scroll(collapseNavbar);
-$(document).ready(collapseNavbar);
+    $(window).scroll(collapseNavbar);
+    $(document).ready(collapseNavbar);
 
 
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+        $('a.page-scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+    $(function() {
+
+
+        $('.flyIn').bind('inview', function(event, visible) {
+            if (visible === true) {
+                $(this).addClass('animated fadeInUp');
+            }
+        });
+
+        $('.flyLeft').bind('inview', function(event, visible) {
+            if (visible === true) {
+                $(this).addClass('animated fadeInLeftBig');
+            }
+        });
+
+        $('.flyRight').bind('inview', function(event, visible) {
+            if (visible === true) {
+                $(this).addClass('animated fadeInRightBig');
+            }
+        });
     });
 });
